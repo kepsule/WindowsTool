a19a30fa17f74de0ad9d728635ac54bccda9e27f
kepsule <kei.i@live.com>
2018-01-14 16:26:50 +0900

    メモツール追加

---
+++ FileCreator/FileCreator/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace FileCreator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.directoryEntry1 = new System.DirectoryServices.DirectoryEntry();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 58);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(533, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 166);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(533, 148);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(228, 345);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 33);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "ファイル作成";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(568, 416);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.DirectoryServices.DirectoryEntry directoryEntry1;
+    }
+}
+
---
+++ FileCreator/FileCreator/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileCreator
+{
+    public partial class Form1 : Form
+    {
+        //保存するフォルダ
+        private const String SavedFolder = @"D:\temp\";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "タイトルを入力してください。";
+            label2.Text = "詳細を入力してください。";
+            textBox1.Focus();
+        }
+
+        //ボタン押下時の動作
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+            //データチェック
+            if (textBox1.Text.Equals(""))
+            {
+                MessageBox.Show("タイトルを入力してください。");
+                return;
+            }
+
+            //エラーない場合、ファイル生成
+            CreateFile(DateTime.Now.ToString("yyyyMMdd_HHmmss"));
+        }
+
+        //ファイル生成、入力処理
+        private void CreateFile(String fileNamePrefix)
+        {
+
+            //ファイル名生成
+            String fileName = SavedFolder + fileNamePrefix + "_" + textBox1.Text + ".txt";
+
+            //ファイル生成
+            System.IO.File.Create(fileName).Close();
+
+            //詳細入力
+            System.IO.StreamWriter sw =
+                new System.IO.StreamWriter(fileName, true,
+                System.Text.Encoding.GetEncoding("shift_jis"));
+            sw.Write(textBox2.Text);
+            sw.Close();
+
+            //完了時の動作
+            MessageBox.Show("保存完了");
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox1.Focus();
+        }
+    }
+}
